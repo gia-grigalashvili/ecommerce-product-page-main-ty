b39ffdf7cc443b7c4b84c46a53801dfd14e7583c
gia-grigalashvili <grigalagia07@gmail.com>
2024-06-16 03:53:25 +0400

    import two img in header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
 import React from "react";
-
-export default function Header() {
-  return <div>Header</div>;
+import styled from "styled-components";
+import Burger from "/public/images/icon-menu.svg";
+import Logo from "/public/images/logo.svg";
+function Header() {
+  return (
+    <Headerdiv>
+      <div>
+        <img src={Burger} alt="" />
+        <img src={Logo} alt="" />
+      </div>
+    </Headerdiv>
+  );
 }
+const Headerdiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export default Header;
